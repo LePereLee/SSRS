c90f08fc31d70f1b7c45e43e9f915342fe2533db
Lee <22920192203994@stu.xmu.edu.cn>
2024-05-29 16:23:29 +0800

    Changes

--- SpringSeatReservation/springboot/src/test/java/com/example/spring_seatreservation/controller/TeacherControllerTest.java
+++ SpringSeatReservation/springboot/src/test/java/com/example/spring_seatreservation/controller/TeacherControllerTest.java
@@ -4,62 +4,47 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RequestMapping("/forum")
-class ForumControllerTest{
+class TeacherControllerTest {
     private MockMvc mvc;
 
     @BeforeEach
     void setUp() {
-        System.out.println("-----ForumControllerTest start-----");
+        System.out.println("----TeacherControllerTest start----");
         // 构建mvc环境
         mvc = MockMvcBuilders.standaloneSetup(new ForumController()).build();
     }
 
     @AfterEach
     void tearDown() {
-        System.out.println("======ForumControllerTest end======");
+        System.out.println("=====TeacherControllerTest end=====");
     }
 
     @Test
-    void getArticle() throws Exception {
+    void getReservation() throws Exception{
         mvc.perform(MockMvcRequestBuilders
-                        .get("/getArticle")
-                        // 设置返回值类型为utf-8，否则默认为ISO-8859-1
-                        .accept(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                                .get("/getArticle")
+                                // 设置返回值类型为utf-8，否则默认为ISO-8859-1
+                                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE)
                         //.param("name", "Tom")
                 )
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 //.andExpect(MockMvcResultMatchers.content().string("Hello Tom!"))
                 .andDo(MockMvcResultHandlers.print());
-
     }
 
     @Test
-    void insertArticle() {
+    void getReservationNeedSub() {
     }
 
     @Test
-    void insertComment() {
-    }
-
-    @Test
-    void getComment() {
-    }
-
-    @Test
-    void getAnnounce() {
+    void subScore() {
     }
 }
